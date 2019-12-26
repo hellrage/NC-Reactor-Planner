a570dcbe1c62c4d2309ccdf73cea62754fbb0372
Hellrage <hellrage.k@gmail.com>
2019-12-26 21:13:12 +0300

    Force config reset with 2.0.29. This is a hotfix and I reuploaded the .exe in its release.

--- NC Reactor Planner/Configuration.cs
+++ NC Reactor Planner/Configuration.cs
@@ -181,7 +181,7 @@ namespace NC_Reactor_Planner
                 System.Windows.Forms.MessageBox.Show("Pre-overhaul configurations aren't supported!\r\nDelete your BetaConfig.json to regenerate a new one.");
                 return false;
             }
-            if(cf.saveVersion < new Version(2, 0, 27, 0))
+            if(cf.saveVersion < new Version(2, 0, 29, 0))
             {
                 System.Windows.Forms.MessageBox.Show("Ignoring old config file as the values have changed, please overwrite BetaConfig.json");
                 return false;
