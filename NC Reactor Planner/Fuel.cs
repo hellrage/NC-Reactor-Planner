b3938cf9b2da5d68b32e66b8594255626d8fbaab
pbaker <piercebaker@hotmail.com>
2020-05-23 17:06:55 -0400

    Better sorting for Fuel

--- NC Reactor Planner/Fuel.cs
+++ NC Reactor Planner/Fuel.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace NC_Reactor_Planner
 {
-    public class Fuel
-    {
+    public class Fuel : IComparable<Fuel>
+	{
         public string Name { get ; private set; }
         public double BaseHeat { get => Configuration.Fuels[Name].BaseHeat; }
         public double FuelTime { get => Configuration.Fuels[Name].FuelTime; }
@@ -26,5 +27,56 @@ namespace NC_Reactor_Planner
         {
             return string.Format("{0}{1}{2}{3}", (Name + (SelfPriming ? "*" : "")).PadRight(14), BaseEfficiency.ToString().PadRight(6), BaseHeat.ToString().PadRight(5), CriticalityFactor.ToString().PadRight(4));
         }
-    }
+
+		private string getModifier()
+		{
+			Match match = Regex.Match(Name, @"\[([a-zA-Z]+)\]");
+			if (match.Success)
+				return match.Groups[0].Value;
+			else
+				return "";
+		}
+
+		private int getIsotope()
+		{
+			Match match = Regex.Match(Name, @"-([0-9]+)");
+			if (match.Success)
+				return Int32.Parse(match.Groups[0].Value);
+			else
+				return -1;
+		}
+
+		private string getElement()
+		{
+			Match match = Regex.Match(Name, @"([a-zA-Z]+)(?!\])\b");
+			if (match.Success)
+				return match.Groups[0].Value;
+			else
+				return "";
+		}
+
+		public int CompareTo(Fuel right)
+		{
+			if (right == null)
+				return 1;
+
+			int leftIsotope = getIsotope();
+			int rightIsotope = right.getIsotope();
+			int compareIsotope = leftIsotope.CompareTo(rightIsotope);
+			if (compareIsotope != 0)
+				return compareIsotope;
+
+			string leftElement = getElement();
+			string rightElement = right.getElement();
+			int compareElement = leftElement.CompareTo(rightElement);
+			if (compareIsotope != 0)
+				return compareIsotope;
+
+			string leftModifier = getModifier();
+			string rightModifier = right.getModifier();
+			int compareModifier = leftModifier.CompareTo(rightModifier);
+
+			return compareModifier;
+		}
+	}
 }
